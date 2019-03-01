ee218870303fd17a1ecbedefe561738e10a33c61
uNitx1337 <45987906+uNitx1337@users.noreply.github.com>
2019-03-01 21:00:03 +0600

    Update for actual offsets

--- Engine.h
+++ Engine.h
@@ -1,5 +1,5 @@
 /*
-	by unitx 12.22.2018
+	by unitx 12.22.2018 (3.1.2019 - lastUpdate)
 */
 
 #pragma once
@@ -40,30 +40,29 @@
 #include <psapi.h>
 
 #include <vector>
-
 #include "Vector3.h"
 
 #pragma warning (disable: 4996)
 
 using namespace std;
 
-
-// it 0.63 offsets u need just update this
 // new offsets -> https://www.unknowncheats.me/forum/dayz-sa/104269-dayz-structs-offsets.html
 
-#define off_world 0xe0fd48
-#define off_networkmanager 0xdbc170
+//this offset for last build 03/01/2019 (march)
+
+#define off_world 0xe339c8
+#define off_networkmanager 0xddff80
 
 #define off_networkmanager_networkclient 0x50
 
 #define off_networkclient_scoreboard 0x10
-#define off_networkclient_playeridentity_size 0x170
+#define off_networkclient_playeridentity_size 0x168
 #define off_networkclient_servername 0x330
 
 #define off_playeridentity_networkid 0x30
-#define off_playeridentity_playername 0x108
+#define off_playeridentity_playername 0x100
 
-#define off_world_camera 0x1B0
+#define off_world_camera 0x1b8
 
 #define off_camera_viewright 0x8
 #define off_camera_viewup 0x14
@@ -73,25 +72,26 @@ using namespace std;
 #define off_camera_projection_d1 0xD0
 #define off_camera_projection_d2 0xDC
 
-#define off_world_nearanimaltable 0xd38
-#define off_world_faranimaltable 0xe88
+#define off_world_nearanimaltable 0xe48
+#define off_world_faranimaltable 0xf90
 #define off_world_slowanimaltable 0x10a8
-#define off_world_itemtable 0x1ea8
-#define off_world_bullettable 0xbe0
-#define off_world_cameraon 0x27c8
-#define off_world_playeron 0x27d0
+#define off_world_itemtable 0x1f48
+#define off_world_bullettable 0xD00 
+
+#define off_world_cameraon 0x2840
+#define off_world_playeron 0x2848
 
 #define off_sortedobject_entity 0x8
 
 #define off_entity_cameraentry 0x2168
-#define off_entity_futurevisualstate 0x78 
-#define off_entity_renderervisualstate 0x120
+#define off_entity_futurevisualstate 0x88 
+#define off_entity_renderervisualstate 0x130
 #define off_entity_futureentitytype 0x38
 #define off_entity_rendererentitytype 0xe0
 #define off_entity_sortobject 0x138
 #define off_entity_baseentity 0xa8
 #define off_entity_networkid 0x564
-#define off_entity_inventory 0x4e0
+#define off_entity_inventory 0x438
 
 #define off_cameraentry_movupdown 0xa4
 #define off_cameraentry_movforwardbackward 0xc4
@@ -104,7 +104,7 @@ using namespace std;
 #define off_entitytype_typename 0x68
 #define off_entitytype_modelname 0x78
 #define off_entitytype_configname 0x98
-#define off_entitytype_cleanname 0x530
+#define off_entitytype_cleanname 0x540
 
 #define off_visualstate_position 0x2c
 #define off_visualstate_headposition 0xf8
